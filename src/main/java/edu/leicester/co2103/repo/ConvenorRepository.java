752ef4bab6892c02f658459c90fe43be61d094da
joshcampbell7 <joshuacampbell49@yahoo.co.uk>
2022-05-09 19:04:14 +0100

    init

--- src/main/java/edu/leicester/co2103/repo/ConvenorRepository.java
+++ src/main/java/edu/leicester/co2103/repo/ConvenorRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.repository.CrudRepository;
 
 import edu.leicester.co2103.domain.Convenor;
 
+import java.util.Optional;
+
 public interface ConvenorRepository extends CrudRepository<Convenor, Long> {
 
 }
